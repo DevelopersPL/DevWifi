2a4316366e0694e5dea461f017a55de8177d1051
Daniel Speichert <daniel@speichert.pro>
2014-03-04 00:34:40 -0500

    MAC - masked input

--- public_html/js/DevWifi.js
+++ public_html/js/DevWifi.js
@@ -6,4 +6,9 @@ $(function() {
             $(this).tab('show');
         });
     }
+
+    // masked input
+    $.mask.definitions['h'] = "[A-Fa-f0-9]";
+    if($("#inputMac").length)
+        $("#inputMac").mask("hh:hh:hh:hh:hh:hh");
 });
